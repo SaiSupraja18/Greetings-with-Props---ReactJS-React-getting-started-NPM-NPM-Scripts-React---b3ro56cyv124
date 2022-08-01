00dac913ea4f5657bc8d7185701d68f736f2fc18
SaiSupraja <102989692+SaiSupraja18@users.noreply.github.com>
2022-08-02 00:20:48 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
+
+
 import React from "react";
 import '../styles/App.css';
 import Welcome from './Welcome';
 
 const App = () => {
   return (
-    <Welcome name="" />
+    <Welcome name={"Supraja"} />
   )
 }
 
